b284304f05decd48e97db1d333157a66197add08
Thien Liu <thien.liuhoang@gmail.com>
2022-01-16 22:10:06 +0700

    Code cleanup

--- app/auth/models.py
+++ app/auth/models.py
@@ -2,7 +2,6 @@ from app import db
 
 # Define a base model for other database tables to inherit
 class Base(db.Model):
-
     __abstract__  = True
 
     id            = db.Column(db.Integer, primary_key=True)
@@ -12,16 +11,14 @@ class Base(db.Model):
 
 # Define a User model
 class User(Base):
-
     __tablename__ = 'auth_user'
 
     # User Name
-    name    = db.Column(db.String(128),  nullable=False)
+    name = db.Column(db.String(128), nullable=False)
 
     # Identification Data: email & password
-    email    = db.Column(db.String(128),  nullable=False,
-                                            unique=True)
-    password = db.Column(db.String(192),  nullable=False)
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(192), nullable=False)
 
     # Authorisation Data: role & status
     role     = db.Column(db.SmallInteger, nullable=False)
@@ -29,7 +26,6 @@ class User(Base):
 
     # New instance instantiation procedure
     def __init__(self, name, email, password):
-
         self.name     = name
         self.email    = email
         self.password = password
